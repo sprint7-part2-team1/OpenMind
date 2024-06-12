2f2f9fd1a1676147b3b16229a5bbc00f14023df9
홍준기 <hongjungi@hongjungiui-noteubug.local>
2024-06-12 20:07:41 +0900

    :sparkles: 카카오톡 공유기능 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
+function KakaoInit() {
+  useEffect(() => {
+    if (window.Kakao) {
+      if (!window.Kakao.isInitialized()) {
+        window.Kakao.init('fc67d626b1c3f9e2bc05016a2ccab46d');
+      }
+    }
+  }, []);
+
+  return null;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <KakaoInit />
     <App />
   </React.StrictMode>
 );
