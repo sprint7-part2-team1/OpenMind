74aeb3a5d5fc7da507bf1ec20cd0ed8309bce72f
홍준기 <hongjungi@hongjungiui-noteubug.local>
2024-06-12 20:07:41 +0900

    :sparkles: 카카오톡 공유기능 추가

--- src/pages/IndividualFeed/ShareLink.jsx
+++ src/pages/IndividualFeed/ShareLink.jsx
@@ -1,14 +1,37 @@
 function ShareLink() {
+  const url = window.location.href;
   const copyToClipboard = () => {
-    const url = window.location.href;
-
     navigator.clipboard.writeText(url);
+  };
+
+  const shareKakaoTalk = () => {
+    window.Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: '오픈마인드',
+        description: '궁금한 내용을 물어보세요!',
+        imageUrl: 'https://via.placeholder.com/300', // 여기에 실제 이미지 URL을 넣으세요
+        link: {
+          webUrl: url,
+          mobileWebUrl: url,
+        },
+      },
+      buttons: [
+        {
+          title: '답변하러 가기',
+          link: {
+            webUrl: url,
+            mobileWebUrl: url,
+          },
+        },
+      ],
+    });
   };
 
   return (
     <div>
       <button onClick={copyToClipboard}>link공유하기</button>
-      <button></button>
+      <button onClick={shareKakaoTalk}>카카오톡으로 공유하기</button>
       <button></button>
     </div>
   );
