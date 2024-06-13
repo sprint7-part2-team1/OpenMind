ea2aa89be9812f3ff0d075cb235a70680e9f3e8a
홍준기 <hongjungi@hongjungiui-noteubug.local>
2024-06-13 16:05:22 +0900

    :lipstick: 헤더 ui 수정, 프로필 이미지 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
-import headerImg from '../assets/images/headerImg.png';
 import styles from './Header.module.css';
 import logo from '../assets/images/logo.png';
-import { Link } from 'react-router-dom';
+import '../global.css';
 
 function Header() {
   return (
     <header className={styles.header}>
       <img className={styles.logo} src={logo} alt='logo' />
+      <img className={styles.profileImg} src={logo} alt='profile-img' />
     </header>
   );
 }
