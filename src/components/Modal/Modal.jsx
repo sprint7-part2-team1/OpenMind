1abc1e4b773a3d64e1c134951aa0e546e7541c22
somang <149379670+som0301@users.noreply.github.com>
2024-06-13 20:38:59 +0900

    :art: refactor: ModalForm 코드 모듈화

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,58 +1,16 @@
 import '../../global.css'; 
-import React, { useEffect, useState } from "react";
-import { createQuestion } from "../../api/modalApi";
 import Icon from '../Icon/Icon'
 import Styles from './Modal.module.css';
+import ModalForm from './ModalForm';
 
 const TEAM = '7-1'
+const subjectId = '6742';
 
-function Modal({onClose}) {
-  const [content, setContent] = useState('');
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [submittingError, setSubmittingError] = useState(null);
-  const [hasContent, setHasContent] = useState(false);
-  const subjectId = '6742';
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
-  }
+function Modal({ onClose }) {
 
   const preventModalClose = (e) => {
     e.stopPropagation();
   };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = {
-      subjectId,
-      content,
-      team: TEAM
-    }
-
-    try {
-      setSubmittingError(null);
-      setIsSubmitting(true);
-      await createQuestion(formData)
-    } catch (error) {
-      setSubmittingError(error);
-      return;
-    } finally {
-      setIsSubmitting(false);
-    }
-    setContent('');
-  }
-
-  const contentButtonOnOff = () => {
-    if (content.trim() !== ''){
-      setHasContent(true);
-    } else {
-      setHasContent(false);
-    };
-  }
-
-  useEffect(() => {
-    contentButtonOnOff();
-  },[content])
   
   return (
     <div className={Styles.modalOverlay} onClick={onClose}>
@@ -66,11 +24,7 @@ function Modal({onClose}) {
           <section>
 
           </section>
-          <form onSubmit={handleSubmit} className={Styles.form}>
-            <textarea value={content} onChange={handleContentChange} placeholder="질문을 입력해주세요"></textarea>
-            <button type="submit" disabled={isSubmitting || !hasContent}>질문 보내기</button>
-            {submittingError?.message && <div>{submittingError.message}</div>}
-          </form>
+          <ModalForm team={TEAM} subjectId={subjectId} onClose={onClose}/>
         </div>
       </div>
     </div>
