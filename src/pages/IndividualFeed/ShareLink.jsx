fa4439e74494f8c3d5037599d6c30036f401f1f0
홍준기 <hongjungi@hongjungiui-noteubug.local>
2024-06-13 14:17:27 +0900

    :sparkles: 토스트 기능 추가

--- src/pages/IndividualFeed/ShareLink.jsx
+++ src/pages/IndividualFeed/ShareLink.jsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from 'react';
+import Toast from './Toast';
+
 function ShareLink() {
   const url = window.location.href;
+  const [toastMessage, setToastMessage] = useState('');
+
   const copyToClipboard = () => {
     navigator.clipboard.writeText(url);
+    setToastMessage('URL이 복사되었습니다.');
+  };
+
+  const closeToast = () => {
+    setToastMessage('');
   };
 
   const shareKakaoTalk = () => {
@@ -29,7 +39,7 @@ function ShareLink() {
   };
 
   const shareFacebook = () => {
-    window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}`);
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}`); // 외부에서 접근 가능한 url이어야 기능 정상 작동.
   };
 
   return (
@@ -37,6 +47,9 @@ function ShareLink() {
       <button onClick={copyToClipboard}>link공유하기</button>
       <button onClick={shareKakaoTalk}>카카오톡으로 공유하기</button>
       <button onClick={shareFacebook}>페이스북으로 공유하기</button>
+      {toastMessage && (
+        <Toast message={toastMessage} duration={5000} onClose={closeToast} />
+      )}
     </div>
   );
 }
