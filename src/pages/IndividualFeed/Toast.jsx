516c4e96f27a5a1247d1265bdf4cf6b50a9f7fd4
홍준기 <hongjungi@hongjungiui-noteubug.local>
2024-06-14 14:37:56 +0900

    :lipstick: 버튼 아이콘 추가

--- src/pages/IndividualFeed/Toast.jsx
+++ src/pages/IndividualFeed/Toast.jsx
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 import styles from './Toast.module.css';
 
 function Toast({ message, duration, onClose }) {
+  const test = () => {
+    console.log('hi');
+  };
   useEffect(() => {
     const timer = setTimeout(onClose, duration);
 
