ea0df6f8b0663868f00723ada201d60a70eddeb4
ddol9 <drddol9@gmail.com>
2024-06-14 17:22:34 +0900

    :sparkles: feat: 피드 카드 리스트생성 및 api 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-function App() {
-  return <></>;
-}
+import AnswerPage from './components/FeedCardList/FeedCardList';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <AnswerPage subjectId='6742' />
+    </div>
+  );
+};
+
 export default App;
