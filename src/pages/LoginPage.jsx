73431046de3330e5e9269d4d36885eff8cdfb285
IllllIIllIIl <dlackdrl0117@gmail.com>
2024-06-15 21:08:09 +0900

    ✨ Feat: LoginPage 컴포넌트 생성
    아직미완

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,35 +7,43 @@ import style from './LoginPage.module.css';
 import React, { useState, useEffect } from 'react';
 
 function LoginPage() {
-  const InputWithLocalStorage = () => {
-    const [inputValue, setInputValue] = useState(() => {
-      // 컴포넌트 마운트 시 로컬 스토리지에서 초기값 가져오기
-      return localStorage.getItem('inputValue') || '';
-    });
-  };
+  // const [nameInput, setNameInput] = useState('');
+  // const [inputValue, setInputValue] = useState('');
+  // const inputRef = useRef();
+
+  // const handleNameInputKeyPress = (e) => {
+  //   if (e.key === 'Enter' && nameInput.trim() !== '') {
+  //     setNameInput();
+  //   }
+  // };
 
   return (
-    <div className={style.header}>
-      <div className={style.header_box}>
-        {/* <Link to='/questionListPage' className={style.header_right}>
-          <a>질문하러 가기</a>
-        </Link> */}
-        <button className={style.header_right}>질문하러 가기</button>
+    <div className={style.container}>
+      <div className={style.header}>
+        <div className={style.header_box}>
+          {/* <Link to='/questionListPage' className={style.header_right}>
+            <a>질문하러 가기</a>
+          </Link> */}
+          <button className={style.header_right}>질문하러 가기</button>
+        </div>
       </div>
-      <div className={style.main}>
-        <img className={style.main_logo} src={Logo} alt='Logo' />
+      <div className={style.make_gap}>
+        <div className={style.main}>
+          <img className={style.main_logo} src={Logo} alt='Logo' />
 
-        <form className={style.form}>
-          <input
-            className={style.nickName_input}
-            placeholder='이름을 입력하세요'
-          ></input>
-          <span className={style.input_personIcon}>
-            <img src={personIcon} alt='personIcon' />
-            {/* <Icon iconName={'Person'} /> */}
-          </span>
-          <button className={style.login_btn}>질문 받기</button>
-        </form>
+          <form className={style.form}>
+            <input
+              className={style.nickName_input}
+              placeholder='이름을 입력하세요'
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <span className={style.input_personIcon}>
+              <img src={personIcon} alt='personIcon' />
+              {/* <Icon iconName={'Person'} /> */}
+            </span>
+            <button className={style.login_btn}>질문 받기</button>
+          </form>
+        </div>
       </div>
       <footer className={style.footer}>
         <img
