6a5d503cf6e5b36489b271966d9349e79805fd19
IllllIIllIIl <dlackdrl0117@gmail.com>
2024-06-15 18:21:16 +0900

    ✨ Feat: LoginPage 생성
    코드 수정중

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,22 +1,50 @@
-import logo from '../assets/images/logo.png';
-import Icon from '../components/Icon/Icon'
-import style from './LoginPage.module.css';
 import '../global.css';
+import { loginPageBackgroundImage, Logo, personIcon } from '../assets/images';
+import Icon from '../components/Icon/Icon';
+import style from './LoginPage.module.css';
+// import { Link } from 'react-router-dom';
+
+import React, { useState, useEffect } from 'react';
 
 function LoginPage() {
-  return(
-    <div className={style.bg}>
-      <div className={style.content}>
-        <a href='#'>질문하러 가기</a>
-        <img src={logo} alt='logo' />
-        <form>
-          <Icon iconName={'Person'}/>
-          <input placeholder='이름을 입력하세요'></input>
-          <button>질문 받기</button>
+  const InputWithLocalStorage = () => {
+    const [inputValue, setInputValue] = useState(() => {
+      // 컴포넌트 마운트 시 로컬 스토리지에서 초기값 가져오기
+      return localStorage.getItem('inputValue') || '';
+    });
+  };
+
+  return (
+    <div className={style.header}>
+      <div className={style.header_box}>
+        {/* <Link to='/questionListPage' className={style.header_right}>
+          <a>질문하러 가기</a>
+        </Link> */}
+        <button className={style.header_right}>질문하러 가기</button>
+      </div>
+      <div className={style.main}>
+        <img className={style.main_logo} src={Logo} alt='Logo' />
+
+        <form className={style.form}>
+          <input
+            className={style.nickName_input}
+            placeholder='이름을 입력하세요'
+          ></input>
+          <span className={style.input_personIcon}>
+            <img src={personIcon} alt='personIcon' />
+            {/* <Icon iconName={'Person'} /> */}
+          </span>
+          <button className={style.login_btn}>질문 받기</button>
         </form>
       </div>
+      <footer className={style.footer}>
+        <img
+          className={style.footer_backgroundImg}
+          src={loginPageBackgroundImage}
+          alt='backgroundImage'
+        />
+      </footer>
     </div>
-  )
+  );
 }
-
 export default LoginPage;
