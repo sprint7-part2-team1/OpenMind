4bd82fab74c3b21dc88024c3c52d8f5587b03345
somang <149379670+som0301@users.noreply.github.com>
2024-06-15 12:40:46 +0900

    :fire: remove: ModalProfile 파일 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from './components/modal/Modal.jsx';
+import useModal from './components/modal/useModal.jsx';
+
 function App() {
-  return <></>;
+  const { isOpen, openModal, closeModal} = useModal();
+  const portalRoot = document.getElementById('portal-root');
+  
+  return (
+    <div>
+      <button onClick={openModal}>질문하러 가기</button>
+      {isOpen && ReactDOM.createPortal(<Modal onClose={closeModal} />, portalRoot)}
+    </div>
+)
 }
 
 export default App;
