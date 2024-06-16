b5fdceb1b220996cbc84ab3c9f3c9f7229c5f287
홍준기 <hongjungi@hongjungiui-noteubug.local>
2024-06-16 11:38:00 +0900

    :bug: conflict 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,33 @@
-
 import styles from './Header.module.css';
 import logo from '../assets/images/logo.png';
 import '../global.css';
+import { getSubjectDetail } from '../api/subjects/subjectsApi';
+import { useEffect, useState } from 'react';
 
+function Header({ subjectId }) {
+  const [userProfileImage, setUserProfileImage] = useState('');
 
-function Header() {
+  useEffect(() => {
+    const fetchSubjectDetail = async () => {
+      try {
+        const subjectDetail = await getSubjectDetail(subjectId);
+        setUserProfileImage(subjectDetail.imageSource);
+      } catch (error) {
+        console.error('Error fetching subject detail:', error);
+      }
+    };
+
+    fetchSubjectDetail();
+  }, [subjectId]);
+
   return (
     <header className={styles.header}>
       <img className={styles.logo} src={logo} alt='logo' />
-
-      <img className={styles.profileImg} src={logo} alt='profile-img' />
-
+      <img
+        className={styles.profileImg}
+        src={userProfileImage}
+        alt='profile-img'
+      />
     </header>
   );
 }
