81972024f08ca90578bda69668f00fd6b4240c0c
홍준기 <hongjungi@hongjungiui-noteubug.local>
2024-06-16 11:11:31 +0900

    :bug: conflict 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
+
 import styles from './Header.module.css';
 import logo from '../assets/images/logo.png';
 import '../global.css';
+
 
 function Header() {
   return (
     <header className={styles.header}>
       <img className={styles.logo} src={logo} alt='logo' />
+
       <img className={styles.profileImg} src={logo} alt='profile-img' />
+
     </header>
   );
 }
