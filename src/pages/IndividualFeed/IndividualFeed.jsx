ae5de8412ace6f7899a771cd41d7a249ea0a5f1e
홍준기 <hongjungi@hongjungiui-noteubug.local>
2024-06-17 20:25:50 +0900

    :lipstick: 개인피드 css 수정

--- src/pages/IndividualFeed/IndividualFeed.jsx
+++ src/pages/IndividualFeed/IndividualFeed.jsx
@@ -7,6 +7,7 @@ import useModal from '../modal/useModal';
 import ReactDOM from 'react-dom';
 import Modal from '../modal/Modal';
 import { useParams } from 'react-router-dom';
+import Button from '../../components/Button/Button';
 
 function IndividualFeed() {
   const { subjectId } = useParams(); // URL 파라미터에서 subjectId를 가져옴
@@ -15,12 +16,13 @@ function IndividualFeed() {
   const portalRoot = document.getElementById('portal-root');
 
   return (
-    <div>
+    <div className={styles['individualFeed-body']}>
       <Header subjectId={subjectId} />
       <div className={styles['feed-container']}>
         <FeedCardList subjectId={subjectId} />
       </div>
-      <button onClick={openModal}>질문하러 가기</button>
+
+      <Button text='WriteQs' onClick={openModal} isDisabled={false} />
       {isOpen &&
         ReactDOM.createPortal(<Modal onClose={closeModal} />, portalRoot)}
     </div>
