5ef51f05a756a1891ae9786560c9cb5c106bcb6d
ddol9 <drddol9@gmail.com>
2024-06-18 16:46:42 +0900

    :sparkles: feat: 답변 페이지 경로 및 답변 작성 칸 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import IndividualFeed from './pages/IndividualFeed/IndividualFeed';
 import List from './pages/ListPages/List';
+import AnswerFeed from './pages/AnswerFeed/AnswerFeed';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/individualFeed/:subjectId' element={<IndividualFeed />} />
+        <Route path='/individualFeed/:subjectId/answer' element={<AnswerFeed />} />
         <Route path='/list' element={<List />} />
       </Routes>
     </Router>
