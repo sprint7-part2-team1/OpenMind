b0ffbf65c58a24dcca77d8294eba6c9aa9a84c90
ddol9 <drddol9@gmail.com>
2024-06-18 17:52:15 +0900

    :lipstick: style: 답변 피드 카드 css 수정

--- src/components/FeedCard/FeedCardAnswerInput.jsx
+++ src/components/FeedCard/FeedCardAnswerInput.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import KebabMenu from '../KebabMenu/KebabMenu';
 import styles from './FeedCardAnswerInput.module.css';
+import Button from '../Button/Button';
 
 const FeedCardAnswerInput = ({
   initialAnswer = '',
@@ -21,18 +22,6 @@ const FeedCardAnswerInput = ({
     onSubmit(currentAnswer);
   };
 
-  const handleEdit = () => {
-    onEdit(currentAnswer);
-  };
-
-  const handleReject = () => {
-    onReject();
-  };
-
-  const handleDelete = () => {
-    onDelete();
-  };
-
   return (
     <div className={styles.feedcardAnswerInput}>
       <div className={styles.header}>
@@ -44,26 +33,16 @@ const FeedCardAnswerInput = ({
         <div className={styles['feedcard-answer-content']}>
           <div className={styles['feedcard-answer-content-header']}>
             <div className={styles['feedcard-user-name']}>{username}</div>
-            <KebabMenu
-              onEdit={handleEdit}
-              onReject={handleReject}
-              onDelete={handleDelete}
+            <textarea
+              value={currentAnswer}
+              onChange={handleAnswerChange}
+              placeholder='답변을 입력하세요'
+              className={styles['feedcard-answer-input']}
             />
           </div>
+          <Button text={'DoneAs'} onClick={handleSubmit} />
         </div>
       </div>
-      <textarea
-        value={currentAnswer}
-        onChange={handleAnswerChange}
-        placeholder='답변을 입력하세요'
-        className={styles['feedcard-answer-input']}
-      />
-      <button
-        className={styles['feedcard-submit-button']}
-        onClick={handleSubmit}
-      >
-        답변 완료
-      </button>
     </div>
   );
 };
