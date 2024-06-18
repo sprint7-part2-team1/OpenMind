59dbcec4b95b8d7daf64f2b0bc5a8066d1258339
IllllIIllIIl <dlackdrl0117@gmail.com>
2024-06-18 17:55:51 +0900

    ✨ Feat: 회원 생성 기능
    회원 생성 시 로컬브랜치에 데이터 저장 및 중복 닉네임 생성 막기

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,19 +8,36 @@ import { postSubject } from '../api/subjects/subjectsApi';
 
 function LoginPage() {
   const [nameInput, setNameInput] = useState('');
+  const [error, setError] = useState(null);
   const [result, setResult] = useState({});
   const navigate = useNavigate();
+
+  const checkDuplicateNickname = (nickname) => {
+    const nicknames = JSON.parse(localStorage.getItem('nicknames')) || [];
+    return nicknames.includes(nickname);
+  };
+
+  const addNickname = (nickname) => {
+    const nicknames = JSON.parse(localStorage.getItem('nicknames')) || [];
+    nicknames.push(nickname);
+    localStorage.setItem('nicknames', JSON.stringify(nicknames));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (nameInput.trim() !== '') {
-      try {
-        const SubjectResult = await postSubject(nameInput);
-        setResult(SubjectResult);
-        console.log(SubjectResult);
-        navigate(`/individualFeed/${SubjectResult.id}`);
-      } catch (error) {
-        console.error('회원생성에 실패했습니다:', error);
+      if (checkDuplicateNickname(nameInput)) {
+        setError('닉네임이 이미 존재합니다.');
+      } else {
+        try {
+          const SubjectResult = await postSubject(nameInput);
+          addNickname(nameInput);
+          setResult(SubjectResult);
+          console.log(SubjectResult);
+          navigate(`/individualFeed/${SubjectResult.id}`);
+        } catch (error) {
+          console.error('회원생성에 실패했습니다:', error);
+        }
       }
     }
   };
@@ -42,9 +59,13 @@ function LoginPage() {
             className={styles.nickName_input}
             value={nameInput}
             placeholder='이름을 입력하세요'
-            onChange={(e) => setNameInput(e.target.value)}
+            onChange={(e) => {
+              setNameInput(e.target.value);
+              setError(null);
+            }}
             onKeyDown={(e) => e.key === 'Enter' && handleSubmit(e)}
           />
+          {error && <p className={styles.error}>{error}</p>}
           <span className={styles.input_personIcon}>
             <img src={personIcon} alt='personIcon' />
           </span>
@@ -54,13 +75,7 @@ function LoginPage() {
         </div>
       </div>
       <div className={styles.empty_box}></div>
-      <footer className={styles.footer}>
-        {/* <img
-              className={styles.footer_backgroundImg}
-              src={loginPageBackgroundImage}
-              alt='backgroundImage'
-            /> */}
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }
