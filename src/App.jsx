5e74581f4edb1dc5681e8a5db76f20241fafcda3
IllllIIllIIl <dlackdrl0117@gmail.com>
2024-06-22 18:02:55 +0900

    ✨ Feat/bgm 2차 수정
    토글 버튼생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
+import React, { useRef, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import IndividualFeed from './pages/IndividualFeed/IndividualFeed';
 import List from './pages/ListPages/List';
 import AnswerFeed from './pages/AnswerFeed/AnswerFeed';
-import ReactAudioPlayer from 'react-audio-player';
+
+// CustomAudioPlayer 컴포넌트 정의
+const CustomAudioPlayer = ({ src }) => {
+  const audioRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlayPause = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  return (
+    <div>
+      <audio ref={audioRef} src={src} />
+      <button onClick={handlePlayPause}>{isPlaying ? 'Pause' : 'Play'}</button>
+    </div>
+  );
+};
 
 function App() {
   return (
     <Router>
-      <ReactAudioPlayer
-        src='/music/starTeaParty.mp3'
-        autoPlay={true}
-        controls
-      />
+      <CustomAudioPlayer src='/music/starTeaParty.mp3' />
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/post/:subjectId' element={<IndividualFeed />} />
