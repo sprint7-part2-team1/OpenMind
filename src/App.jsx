51d35ec5e3c96639b18e32110def892fc387cf26
IllllIIllIIl <dlackdrl0117@gmail.com>
2024-06-22 17:57:38 +0900

    ✨ Feat/bgm 1차
    css 꾸미기 남음

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import LoginPage from './pages/LoginPage';
 import IndividualFeed from './pages/IndividualFeed/IndividualFeed';
 import List from './pages/ListPages/List';
 import AnswerFeed from './pages/AnswerFeed/AnswerFeed';
+import ReactAudioPlayer from 'react-audio-player';
 
 function App() {
   return (
     <Router>
+      <ReactAudioPlayer
+        src='/music/starTeaParty.mp3'
+        autoPlay={true}
+        controls
+      />
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/post/:subjectId' element={<IndividualFeed />} />
